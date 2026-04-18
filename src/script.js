4f9a167f76ca2dc3db7740db2a62612efedea090
danielibarr <ddanielibarr@gmail.com>
2026-04-18 19:02:48 -0400

    Fix JavaScript error identified via DevTools

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 let projectName = "WDDM 126 Final Project";
 let projectTitle = "WDDM 126 Final Project";
+let notDefinedVariable = "Fixed!";
+console.log(notDefinedVariable);
 
 function greet() {
     console.log(`Welcome to ${projectName}!`);
